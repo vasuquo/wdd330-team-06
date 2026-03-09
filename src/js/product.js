8c36fb3fb8831fdbda657025a0353a0f172ce565
vasuquo <veasuquo@gmail.com>
2026-03-09 14:07:40 +0100

    2nd Pdetails commit

--- src/js/product.js
+++ src/js/product.js
@@ -3,10 +3,9 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam('product');
+const productId = getParam("product");
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
 
 /* Moved to ProductDetails.mjs
  function addProductToCart(product) {
@@ -27,4 +26,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-*/  
+*/
